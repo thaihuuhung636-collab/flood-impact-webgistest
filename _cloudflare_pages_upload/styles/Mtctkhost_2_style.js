d20aa41d1837292fe058e834988a75b115214155
thaihuuhung636-collab <thaihuuhung636@gmail.com>
2026-06-13 14:44:20 +0700

    Update _cloudflare_pages_upload/styles/Mtctkhost_2_style.js

---
+++ _cloudflare_pages_upload/styles/Mtctkhost_2_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_Mtctkhost_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "7.800000000000001px \'Times New Roman\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'line';
+    if (feature.get("Ten") !== null) {
+        labelText = String(feature.get("Ten"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(255,0,27,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 3.8}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
